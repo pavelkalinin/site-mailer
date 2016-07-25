abb6e009bc85f4783491d95f9935273ab6523e85
pavelkalinin <pavel13kalinin@gmail.com>
2016-07-25 10:17:49 +0500

    Initialize

---
+++ src/main/java/xyz/enhorse/site/Mailer.java
@@ -0,0 +1,12 @@
+package xyz.enhorse.site;
+
+/**
+ * @author <a href="mailto:pavel13kalinin@gmail.com">Pavel Kalinin</a>
+ *         25.07.2016
+ */
+public class Mailer {
+
+    public static void main(String[] args) {
+
+    }
+}
