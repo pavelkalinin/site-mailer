3465ab731566dc23e15ef73fd71d15410a405c11
pavelkalinin <pavel13kalinin@gmail.com>
2016-07-27 14:21:11 +0500

    Remove recipient field from MailMessage

--- src/main/java/xyz/enhorse/site/MailMessage.java
+++ src/main/java/xyz/enhorse/site/MailMessage.java
@@ -8,22 +8,15 @@ import xyz.enhorse.commons.Validate;
  */
 public class MailMessage {
 
-    private final Email recipient;
     private final Email sender;
     private final String subject;
     private final String message;
 
 
-    private MailMessage(final Email recipient, final String sender, final String subject, final String message) {
-        this.recipient = Validate.notNull("mail message recipient", recipient);
+    private MailMessage(final String sender, final String subject, final String message) {
         this.sender = Email.parse(sender);
         this.subject = Validate.defaultIfNull(subject, "It sent from site");
         this.message = Validate.defaultIfNull(message, "The message is empty");
-    }
-
-
-    public Email recipient() {
-        return recipient;
     }
 
 
@@ -44,8 +37,7 @@ public class MailMessage {
 
     @Override
     public int hashCode() {
-        int result = recipient.hashCode();
-        result = 31 * result + sender.hashCode();
+        int result = sender.hashCode();
         result = 31 * result + subject.hashCode();
         result = 31 * result + message.hashCode();
 
@@ -61,8 +53,7 @@ public class MailMessage {
 
         MailMessage that = (MailMessage) o;
 
-        return recipient.equals(that.recipient)
-                && sender.equals(that.sender)
+        return sender.equals(that.sender)
                 && subject.equals(that.subject)
                 && message.equals(that.message);
     }
@@ -70,46 +61,43 @@ public class MailMessage {
 
     @Override
     public String toString() {
-        return "from:\'" + sender()
-                + "\' to:\'" + recipient()
-                + "\'\n\"" + subject()
-                + "\"\n" + message();
+        return "from:\'" + sender() + "\'\n"
+                + "\"" + subject() + "\"\n"
+                + message();
     }
 
 
     public static class Builder {
 
-        private Email recipient;
         private String sender;
         private String subject;
         private String message;
 
 
-        public Builder(final Email recipient) {
-            this.recipient = recipient;
+        public Builder() {
         }
 
 
-        Builder addSender(final String sender) {
+        public Builder addSender(final String sender) {
             this.sender = sender;
             return this;
         }
 
 
-        Builder addSubject(final String subject) {
+        public Builder addSubject(final String subject) {
             this.subject = subject;
             return this;
         }
 
 
-        Builder addMessage(final String message) {
+        public Builder addMessage(final String message) {
             this.message = message;
             return this;
         }
 
 
-        MailMessage build() {
-            return new MailMessage(recipient, sender, subject, message);
+        public MailMessage build() {
+            return new MailMessage(sender, subject, message);
         }
     }
 }
