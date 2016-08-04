d13d19939ea20144409bf381ac36f6f6848f8816
pavelkalinin <pavel13kalinin@gmail.com>
2016-08-04 11:12:13 +0500

    Divide method MailMessage.Builder.addMessage() into .setContent() and .addContent()

--- src/main/java/xyz/enhorse/site/MailMessage.java
+++ src/main/java/xyz/enhorse/site/MailMessage.java
@@ -11,22 +11,22 @@ import java.nio.charset.Charset;
 public class MailMessage {
 
     private final String name;
-    private final String email;
+    private final String address;
     private final String subject;
     private final String content;
-    private final String charset;
+    private final String encoding;
 
 
     private MailMessage(final String name,
-                        final String email,
+                        final String address,
                         final String subject,
                         final String content,
-                        final String charset) {
-        this.name = Validate.required("name", name);
-        this.email = Validate.required("email", email);
-        this.subject = Validate.required("subject", subject);
-        this.content = Validate.required("content", content);
-        this.charset = Validate.defaultIfNullOrEmpty(charset, Charset.defaultCharset().name());
+                        final String encoding) {
+        this.name = Validate.defaultIfNull(name, "");
+        this.address = Validate.required("address", address);
+        this.subject = Validate.defaultIfNull(subject, "");
+        this.content = Validate.defaultIfNull(content, "");
+        this.encoding = Validate.defaultIfNullOrEmpty(encoding, Charset.defaultCharset().name());
     }
 
 
@@ -35,8 +35,8 @@ public class MailMessage {
     }
 
 
-    public String email() {
-        return email;
+    public String address() {
+        return address;
     }
 
 
@@ -50,15 +50,15 @@ public class MailMessage {
     }
 
 
-    public String charset() {
-        return charset;
+    public String encoding() {
+        return encoding;
     }
 
 
     @Override
     public int hashCode() {
         int result = name.hashCode();
-        result = 31 * result + email.hashCode();
+        result = 31 * result + address.hashCode();
         result = 31 * result + subject.hashCode();
         result = 31 * result + content.hashCode();
 
@@ -75,63 +75,72 @@ public class MailMessage {
         MailMessage that = (MailMessage) o;
 
         return name.equals(that.name)
-                && email.equals(that.email)
+                && address.equals(that.address)
                 && subject.equals(that.subject)
-                && content.equals(that.content);
+                && content.equals(that.content)
+                && encoding.equals(that.encoding);
     }
 
 
     @Override
     public String toString() {
-        return String.format("\'%s\'<%s>%n\'%s\'%n\"%s\"", name(), email(), subject(), content());
+        return String.format("Encoding: %s%nFrom: \'%s\' <%s>%nSubject: \'%s\'%n%s",
+                encoding(), name(), address(), subject(), content());
     }
 
 
     public static class Builder {
 
         private String name;
-        private String email;
+        private String address;
         private String subject;
-        private String message;
-        private String charset;
+        private StringBuilder content = new StringBuilder();
+        private String encoding;
 
 
         public Builder() {
         }
 
 
-        public Builder addName(final String name) {
+        public Builder setName(final String name) {
             this.name = name;
             return this;
         }
 
 
-        public Builder addEmail(final String email) {
-            this.email = email;
+        public Builder setAddress(final String address) {
+            this.address = address;
             return this;
         }
 
 
-        public Builder addSubject(final String subject) {
+        public Builder setSubject(final String subject) {
             this.subject = subject;
             return this;
         }
 
 
-        public Builder addMessage(final String message) {
-            this.message = message;
+        public Builder setEncoding(final String encoding) {
+            this.encoding = encoding;
             return this;
         }
 
 
-        public Builder setCharset(final String charset) {
-            this.charset = charset;
+        public Builder addContent(final String string) {
+            content.append(string);
+            return this;
+        }
+
+
+        public Builder setContent(final String string) {
+            content.setLength(0);
+            content.append(string);
             return this;
         }
 
 
         public MailMessage build() {
-            return new MailMessage(name, email, subject, message, charset);
+            return new MailMessage(name, address, subject, content.toString(), encoding);
         }
     }
 }
