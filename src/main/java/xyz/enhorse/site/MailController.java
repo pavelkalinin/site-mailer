c87f1c047e37bcf2db67700fb88c772ad56d1360
pavelkalinin <pavel13kalinin@gmail.com>
2016-08-04 14:11:21 +0500

    Add feature: sending email to admin if exception was happen

--- src/main/java/xyz/enhorse/site/MailController.java
+++ src/main/java/xyz/enhorse/site/MailController.java
@@ -7,7 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 /**
  * @author <a href="mailto:pavel13kalinin@gmail.com">Pavel Kalinin</a>
@@ -15,9 +19,15 @@ import java.nio.charset.StandardCharsets;
  */
 public class MailController extends HttpServlet {
 
+    private static final String FORM_NAME = "name";
+    private static final String FORM_EMAIL = "email";
+    private static final String FORM_SUBJECT = "subject";
+    private static final String FORM_CONTENT = "content";
+
     private final MailService service;
     private final String redirectToSuccess;
     private final String redirectToFail;
+    private final String admin;
 
 
     public MailController(final Configuration configuration) {
@@ -25,6 +35,7 @@ public class MailController extends HttpServlet {
         service = new MailService(configuration);
         redirectToSuccess = Validate.notNull("redirect to if success URL", configuration.redirectToSuccess());
         redirectToFail = Validate.notNull("redirect to if fail URL", configuration.redirectToFail());
+        admin = Validate.defaultIfNull("admin's email address", configuration.adminAddress());
     }
 
 
@@ -37,6 +48,7 @@ public class MailController extends HttpServlet {
             response.sendRedirect(redirectToSuccess);
             response.setStatus(HttpServletResponse.SC_ACCEPTED);
         } catch (Exception ex) {
+            service.sendMail(generateMailToAdmin(ex.getMessage(), stackTraceToString(ex)));
             response.sendRedirect(redirectToFail);
             response.setStatus(HttpServletResponse.SC_EXPECTATION_FAILED);
         }
@@ -44,20 +56,44 @@ public class MailController extends HttpServlet {
 
 
     private MailMessage generateMail(final HttpServletRequest request) {
-        Validate.notNull("request", request);
-
         String charset = Validate.defaultIfNull(request.getCharacterEncoding(), StandardCharsets.UTF_8.name());
-        String name = request.getParameter("name");
-        String email = request.getParameter("email");
-        String subject = request.getParameter("subject");
-        String content = request.getParameter("content");
+        String name = request.getParameter(FORM_NAME);
+        String email = request.getParameter(FORM_EMAIL);
+        String subject = request.getParameter(FORM_SUBJECT);
+        String content = request.getParameter(FORM_CONTENT);
 
         return new MailMessage.Builder()
-                .addName(name)
-                .addEmail(email)
-                .addSubject(subject)
-                .addMessage(content)
-                .setCharset(charset)
+                .setName(name)
+                .setAddress(email)
+                .setSubject(subject)
+                .setContent(content)
+                .setEncoding(charset)
                 .build();
+    }
+
+
+    private MailMessage generateMailToAdmin(final String error, String cause) {
+        return new MailMessage.Builder()
+                .setAddress(admin)
+                .setSubject("error: " + new Date())
+                .setContent(error)
+                .addContent(System.lineSeparator() + cause)
+                .setEncoding(Charset.defaultCharset().name())
+                .build();
+    }
+
+
+    private static String stackTraceToString(final Exception exception) {
+        String stacktrace;
+
+        try (StringWriter writer = new StringWriter();
+             PrintWriter printer = new PrintWriter(writer, true)) {
+            exception.printStackTrace(printer);
+            stacktrace = writer.toString();
+        } catch (Exception ex) {
+            stacktrace = "Can't print stacktrace:\'" + ex.getMessage() + "\'";
+        }
+
+        return stacktrace;
     }
 }
